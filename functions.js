ede1076fb4136eb443ddcad63bcc168b7d33b8d4
Ashwin Gupta <zi78494@umbc.edu>
2023-10-30 21:12:37 -0400

    functional

--- functions.js
+++ functions.js
@@ -1,13 +1,32 @@
+//allowed applications' names
 var texts = ["chrome", "gmail", "music"];
-var keys= document.getElementById('key')
-var but=document.getElementById('sb')
+//getting key of the element
+var keys= document.getElementById('key');
+//options in the dropdown of label modality 
+var mods = ["default", "min_info", "linear", "html"];
+//getting key of modality element
+var app = document.getElementById('mod');
+//key for the submit button
+var but=document.getElementById('sb');
 
+//function for displaying the label on click
 function label(){
-    if(texts.indexOf(key.value)==-1){
-        document.write(Keys.value+" <b>Not Found</b>");
-    }
-    else if (texts.indexOf(key.value)==0||1||2){
-        document.write('<div style="text-align:center"><img src="/Users/ashwingupta/Desktop/ESL_research/designs/linear.png" style="width:750px;height:300px;" alt="Google Chrome Linear Design"></div>');
+    //application name input caseless 
+    var appName = (key.value).toLowerCase();
+    //application name not entered wrong or empty submission
+    if(texts.indexOf(appName)==-1){
+        document.write(keys.value+" <b> Please enter the name of an application</b>");
+    } 
+    //application name allowed and default label design selected
+    else  {
+        if(mods.indexOf(app.value)=== 0||mods.indexOf(app.value)===1||mods.indexOf(app.value)===3){
+            document.write('<div style="text-align:center"><img src="/Users/ashwingupta/eclipse-workspace/NutriApp-Feedback-Website/labels/minimum.png" style="width:500px;height:700px;" alt="Google Chrome Linear Design"></div>');
+        }
+        //application name allowed and linear label design selected
+        else{
+            document.write('<div style="text-align:center"><img src="/Users/ashwingupta/eclipse-workspace/NutriApp-Feedback-Website/labels/linear.png" style="width:750px;height:300px;" alt="Google Chrome Linear Design"></div>');
+        } 
     }
 }
+//click event trigger
 but.addEventListener('click', label);
